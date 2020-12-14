08b1e2c682d765ec58795e3c9ec9fcc5fd985fde
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-14 04:12:07 +0100

    podstawianie wspolrzednych

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 
 function showTemperature(response){
-console.log(response);
+  let temperature =Math.round(response.data.main.temp);
+let city = response.data.name
+console.log('The temperature in ${city} is ${temperature}C');
 
 
 }
 
 
 let apiKey = "4a4fe35e6c00e25f675f5b029f252096 ";
-
+let units = "metric";
+let city= "sydney";
 let apiUrl =
- "https;//api.openweathermap.org/data/2.5/weather?q=sidney&appid=4a4fe35e6c00e25f675f5b029f252096&units=metric/";
-axios.get(apiUrl).then(showTemperature)
+ 'https;//api.openweathermap.org/data/2.5/weather?q=&{sydney}&appid=&{apiKey}7units=&{units}';
+
+ axios.get(apiUrl).then(showTemperature)
