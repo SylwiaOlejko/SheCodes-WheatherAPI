63c4a0263fbb93f2366f89a535ff04d8ba4540c6
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-12-14 04:04:11 +0100

    api response sydney, metrics unit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 
+function showTemperature(response){
+console.log(response);
+
+
+}
+
+
 let apiKey = "4a4fe35e6c00e25f675f5b029f252096 ";
+
+let apiUrl =
+ "https;//api.openweathermap.org/data/2.5/weather?q=sidney&appid=4a4fe35e6c00e25f675f5b029f252096&units=metric/";
+axios.get(apiUrl).then(showTemperature)
